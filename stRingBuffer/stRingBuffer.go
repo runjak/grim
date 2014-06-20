155ed2cfb4165fb5665bf84de0a4e5915d1189a2
Jakob Runge <sicarius@g4t3.de>
2014-06-20 09:31:23 +0200

    Ran gofmt on grim/stRingBuffer
    - Where are my manners?

--- stRingBuffer/stRingBuffer.go
+++ stRingBuffer/stRingBuffer.go
@@ -11,8 +11,8 @@ type StRingBuffer struct {
 	start int
 	end   int
 	lines []string
-  // Mainly so we can distinguish empty and full:
-  length int
+	// Mainly so we can distinguish empty and full:
+	length int
 }
 
 func NewStRingBuffer(size int) *StRingBuffer {
@@ -24,103 +24,103 @@ func (s *StRingBuffer) String() string {
 }
 
 func (s *StRingBuffer) Full() bool {
-  return s.length == len(s.lines)
+	return s.length == len(s.lines)
 }
 
 func (s *StRingBuffer) Empty() bool {
-  return s.length == 0
+	return s.length == 0
 }
 
 func (s *StRingBuffer) Length() int {
-  return s.length
+	return s.length
 }
 
 func (s *StRingBuffer) Push(line string) *StRingBuffer {
 	//Adjusting start, iff s.Full():
 	if s.Full() {
-    s.start = s.mod(s.start + 1)
-	}else{
-    s.length++
-  }
+		s.start = s.mod(s.start + 1)
+	} else {
+		s.length++
+	}
 	//Writing values:
-  s.end = s.mod(s.end + 1)
+	s.end = s.mod(s.end + 1)
 	s.lines[s.end] = line
-  //Return for chaining:
-  return s
+	//Return for chaining:
+	return s
 }
 
 func (s *StRingBuffer) Pop() string {
-  if s.Empty() {
-    return ""
-  } else {
-    s.length--
-  }
+	if s.Empty() {
+		return ""
+	} else {
+		s.length--
+	}
 	ret := s.lines[s.end]
-  s.lines[s.end] = ""
-  s.end = s.mod(s.end - 1)
+	s.lines[s.end] = ""
+	s.end = s.mod(s.end - 1)
 	return ret
 }
 
 func (s *StRingBuffer) Unshift(line string) *StRingBuffer {
 	//Adjusting end, iff s.Full():
 	if s.Full() {
-    s.end = s.mod(s.end - 1)
+		s.end = s.mod(s.end - 1)
 	} else {
-    s.length++
-  }
+		s.length++
+	}
 	//Writing values
 	s.lines[s.start] = line
-  s.start = s.mod(s.start - 1)
-  //Return for chaining:
-  return s
+	s.start = s.mod(s.start - 1)
+	//Return for chaining:
+	return s
 }
 
 func (s *StRingBuffer) Shift() string {
-  if s.Empty() {
-    return ""
-  } else {
-    s.length--
-  }
-  s.start = s.mod(s.start + 1)
+	if s.Empty() {
+		return ""
+	} else {
+		s.length--
+	}
+	s.start = s.mod(s.start + 1)
 	ret := s.lines[s.start]
-  s.lines[s.start] = ""
+	s.lines[s.start] = ""
 	return ret
 }
 
 func (s *StRingBuffer) Map(f func(string) string) *StRingBuffer {
-  l := s.Length()
-  for i := 0; i < l; i++ {
-    s.Push(f(s.Shift()))
-  }
-  return s
+	l := s.Length()
+	for i := 0; i < l; i++ {
+		s.Push(f(s.Shift()))
+	}
+	return s
 }
 
 func (s *StRingBuffer) MapR(f func(string) string) *StRingBuffer {
-  l := s.Length()
-  for i := 0; i < l; i++ {
-    s.Unshift(f(s.Pop()))
-  }
-  return s
+	l := s.Length()
+	for i := 0; i < l; i++ {
+		s.Unshift(f(s.Pop()))
+	}
+	return s
 }
 
 func (s *StRingBuffer) Each(f func(string)) *StRingBuffer {
-  return s.Map(mkId(f))
+	return s.Map(mkId(f))
 }
 
 func (s *StRingBuffer) EachR(f func(string)) *StRingBuffer {
-  return s.MapR(mkId(f))
+	return s.MapR(mkId(f))
 }
 
 // Helper function for the two Each functions
-func mkId(f func(string)) (func(string) string) {
-  return func(s string) string {
-    f(s)
-    return s
-  }
+func mkId(f func(string)) func(string) string {
+	return func(s string) string {
+		f(s)
+		return s
+	}
 }
 
 // By adding the len to x, we make sure to stay >= 0
-func (s *StRingBuffer) mod (x int) int {
-  l := len(s.lines)
-  return (x+l) % l
+func (s *StRingBuffer) mod(x int) int {
+	l := len(s.lines)
+	return (x + l) % l
 }
--- stRingBuffer/stRingBuffer_test.go
+++ stRingBuffer/stRingBuffer_test.go
@@ -1,92 +1,92 @@
 package stRingBuffer_test
 
 import (
-  "fmt"
-  "testing"
-  "strconv"
+	"fmt"
+	"strconv"
+	"testing"
 )
 
 import "grim/stRingBuffer"
 
 func TestInfo(t *testing.T) {
-  //Starting with an empty buffer:
+	//Starting with an empty buffer:
 	srb := stRingBuffer.NewStRingBuffer(5)
 	if !srb.Empty() {
-    t.Errorf("StRingBuffer was not empty after NewStRingBuffer(x).")
+		t.Errorf("StRingBuffer was not empty after NewStRingBuffer(x).")
 	}
-  //Pushing some elements:
-  srb.Push("1").Push("2").Push("3").Push("4").Push("5")
-  l := srb.Length()
-  if l != 5 {
-    t.Errorf("StRingBuffer didn't grow to expected Length(), but to %i.", l)
-  }
-  //Buffer should be full:
-  if !srb.Full() {
-    t.Errorf("StRingBuffer isn't Full(), but we expected it to be.")
-  }
-  //String output:
-  want := "StRingBuffer {start: 0, end: 0, lines: [5 1 2 3 4], length: 5}"
-  get  := fmt.Sprintf("%s", srb) // Mainly so that 'fmt' can be imported.
-  if get != want {
-    t.Errorf("StRingBuffer String() test failed with get='%s'.\n", get)
-  }
+	//Pushing some elements:
+	srb.Push("1").Push("2").Push("3").Push("4").Push("5")
+	l := srb.Length()
+	if l != 5 {
+		t.Errorf("StRingBuffer didn't grow to expected Length(), but to %i.", l)
+	}
+	//Buffer should be full:
+	if !srb.Full() {
+		t.Errorf("StRingBuffer isn't Full(), but we expected it to be.")
+	}
+	//String output:
+	want := "StRingBuffer {start: 0, end: 0, lines: [5 1 2 3 4], length: 5}"
+	get := fmt.Sprintf("%s", srb) // Mainly so that 'fmt' can be imported.
+	if get != want {
+		t.Errorf("StRingBuffer String() test failed with get='%s'.\n", get)
+	}
 }
 
 func TestAddTake(t *testing.T) {
-  srb := stRingBuffer.NewStRingBuffer(3)
-  //Testing Push/Pop:
-  srb.Push("1").Push("2").Push("3").Push("4")
-  s := ""
-  for !srb.Empty() {
-    s += srb.Pop()
-  }
-  if s != "432" {
-    t.Errorf("StRingBuffer Pop() didn't reverse Push(), and gave '%s'.", s)
-  }
-  //Testing Un-/Shift:
-  srb.Unshift("4").Unshift("3").Unshift("2").Unshift("1")
-  s = ""
-  for !srb.Empty() {
-    s += srb.Shift()
-  }
-  if s != "123" {
-    t.Errorf("StRingBuffer Shift() didn't reverse Unshift(), and gave '%s'.", s)
-  }
-  //Testing Push/Shift:
-  srb.Push("1").Push("2").Push("3").Push("4")
-  s = ""
-  for !srb.Empty() {
-    s += srb.Shift()
-  }
-  if s != "234" {
-    t.Errorf("StRingBuffer Shift() didn't hold the last Push() in order, but '%s'.", s)
-  }
-  //Testing Unshift/Pop:
-  srb.Unshift("4").Unshift("3").Unshift("2").Unshift("1")
-  s = ""
-  for !srb.Empty() {
-    s += srb.Pop()
-  }
-  if s != "321" {
-    t.Errorf("StRingBuffer Pop() didn't hold the last Unshift() in order, but '%s'.", s)
-  }
+	srb := stRingBuffer.NewStRingBuffer(3)
+	//Testing Push/Pop:
+	srb.Push("1").Push("2").Push("3").Push("4")
+	s := ""
+	for !srb.Empty() {
+		s += srb.Pop()
+	}
+	if s != "432" {
+		t.Errorf("StRingBuffer Pop() didn't reverse Push(), and gave '%s'.", s)
+	}
+	//Testing Un-/Shift:
+	srb.Unshift("4").Unshift("3").Unshift("2").Unshift("1")
+	s = ""
+	for !srb.Empty() {
+		s += srb.Shift()
+	}
+	if s != "123" {
+		t.Errorf("StRingBuffer Shift() didn't reverse Unshift(), and gave '%s'.", s)
+	}
+	//Testing Push/Shift:
+	srb.Push("1").Push("2").Push("3").Push("4")
+	s = ""
+	for !srb.Empty() {
+		s += srb.Shift()
+	}
+	if s != "234" {
+		t.Errorf("StRingBuffer Shift() didn't hold the last Push() in order, but '%s'.", s)
+	}
+	//Testing Unshift/Pop:
+	srb.Unshift("4").Unshift("3").Unshift("2").Unshift("1")
+	s = ""
+	for !srb.Empty() {
+		s += srb.Pop()
+	}
+	if s != "321" {
+		t.Errorf("StRingBuffer Pop() didn't hold the last Unshift() in order, but '%s'.", s)
+	}
 }
 
 func TestMapEach(t *testing.T) {
-  srb := stRingBuffer.NewStRingBuffer(3)
-  srb.Push(".").Push(".").Push(".")
-  c := 0
-  f := func(s string) string {
-    c++
-    return s + strconv.Itoa(c)
-  }
-  srb.Map(f).MapR(f)
-  sum := ""
-  g := func(s string) {
-    sum += s
-  }
-  srb.Each(g).EachR(g)
-  if sum != ".16.25.34.34.25.16" {
-    t.Errorf("StRingBuffer MapEach test didn't hold the expected result but '%s'.\n", sum)
-  }
+	srb := stRingBuffer.NewStRingBuffer(3)
+	srb.Push(".").Push(".").Push(".")
+	c := 0
+	f := func(s string) string {
+		c++
+		return s + strconv.Itoa(c)
+	}
+	srb.Map(f).MapR(f)
+	sum := ""
+	g := func(s string) {
+		sum += s
+	}
+	srb.Each(g).EachR(g)
+	if sum != ".16.25.34.34.25.16" {
+		t.Errorf("StRingBuffer MapEach test didn't hold the expected result but '%s'.\n", sum)
+	}
 }
